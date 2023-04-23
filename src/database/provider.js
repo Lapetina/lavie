b3b2cadbe8c374b0bbc0bee609ed735cfa87ec1c
lapetina <victor.lrs95@gmail.com>
2023-04-23 17:45:58 -0300

    fix: adjust Sequelize class

--- src/database/provider.js
+++ src/database/provider.js
@@ -1,26 +1,17 @@
-import settings from './settings';
-const Sequelize = require("sequelize")
+const { Sequelize } = require('sequelize');
+const settings = require('../settings');
 
-/*
-const DB_NAME = "laviemain";
-const DB_USER = "root";
-const DB_PASSWORD = "Nic22@";
-const DB_CONFIG = {
-    dialect: "mysql",
-    host: "localhost",
-    port: 3306,
-}
-*/
 
-const db_settings = {
+const dbSettings = {
     dbName: settings.dbName,
     dbUser: settings.dbUser,
     dbPassword: settings.dbPassword,
     dbConfig: settings.dbConfig
 }
 
+let db = {}
 try{
-    db = new Sequelize(db_settings);
+    db = new Sequelize(database=settings.dbName, username=settings.dBUser, password=settings.dbPassword, options=settings.dbConfig);
 } catch (error) {
     console.log(error);
 }
@@ -30,7 +21,7 @@ async function hasConnection() {
         await db.authenticate();
         console.log("Banco de dados conectados!");
     } catch(error){
-            console.error("erro ao tentar se conectar com o banco de dados")
+        console.error("erro ao tentar se conectar com o banco de dados")
     }
 }
 
